f477aea497f67e5a5592824a206525a322ad5fbd
emanuel.craciun <emanuel.craciun@icloud.com>
2023-08-22 21:29:19 +0300

    first file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print('Hello World')
+
+print("Hello World", end='')
+
+print('Hello World1',    'Hello World2','Hello World3', sep='#')
+
+# print('Hello World") # - syntax error
+
+# print(HelloWorld) # - semantic error
+
+# Exercise 1
+
+# ---
+#|   |
+# ---
