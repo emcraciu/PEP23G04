187431dd3f42cdda90d48d811803432b82f1af9c
emanuel.craciun <emanuel.craciun@icloud.com>
2023-08-24 19:04:11 +0300

    modul 2

---
+++ modul2_1/part1.py
@@ -0,0 +1,20 @@
+name = 'Emanuel'  # string
+print(name)
+
+age = 37  # int
+print(age)
+result = type(age)
+print(result)
+
+age = 37.0  # float
+print(age)
+result = type(age)
+print(result)
+
+age = 37 + 0j  # complex
+print(age)
+result = type(age)
+print(result)
+
+
+# print(type(37))
