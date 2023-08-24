dfe1cc1ad82e791c3daf8c70e53f55ebd63ac552
emanuel.craciun <emanuel.craciun@icloud.com>
2023-08-24 19:44:24 +0300

    modul 2

--- modul2_1/part1.py
+++ modul2_1/part1.py
@@ -16,5 +16,19 @@ print(age)
 result = type(age)
 print(result)
 
+print('Methods of string: ', dir(name))
 
+# name.
+# age.
 # print(type(37))
+
+
+number1 = 5
+number2 = number1
+number2 = number1 + 1
+print(number2)
+print(number1)
+
+print('Return from print: ', print(37))
+input('Input data here: ')
+# pauza
