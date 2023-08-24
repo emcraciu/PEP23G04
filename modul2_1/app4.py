fda9b166d82a792abff0d61e3be4a217eb0f72d1
emanuel.craciun <emanuel.craciun@icloud.com>
2023-08-24 21:37:22 +0300

    modul 2

---
+++ modul2_1/app4.py
@@ -0,0 +1,9 @@
+venit = input('Enter venit here: ')
+
+print('Recomandarile noastre: ')
+cheltuieli = 50 * int(venit) / 100
+print('Cheltuieli uzuale: ', cheltuieli)
+recreere = 30 * int(venit) / 100
+print('Recreere: ', recreere)
+economii = 20 * int(venit) / 100
+print('Economii si datorii: ', economii)
