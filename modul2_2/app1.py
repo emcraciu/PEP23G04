b49bd5fc1accc1d76196fff68a2bc49870f30431
emanuel.craciun <emanuel.craciun@icloud.com>
2023-08-29 19:29:06 +0300

    modul 2

---
+++ modul2_2/app1.py
@@ -0,0 +1,13 @@
+# 01P - Theodor
+
+# a)
+
+print("Astazi ma duc la \"facultate\". ")
+
+# b)
+
+print('/*\/*\*/*\/*\ \n Python \n \./\./\./\./')
+
+# c)
+
+print('P', 'y', 't', 'h', 'o', 'n', sep='\t')
