150202128b1896a0477f072a54b2bac6cb5b5e6b
emanuel.craciun <emanuel.craciun@icloud.com>
2023-08-29 18:59:43 +0300

    modul 2

---
+++ modul2_1/part2.py
@@ -0,0 +1,17 @@
+my_added_string = 'add me'
+string1 = 'Hello World\n'
+string2 = r'Hello World\n'
+string3 = '''
+
+Hello World
+
+'''
+string4 = f'Hello World {my_added_string}'
+string5 = rf"\nHello World {my_added_string}"
+
+print(string1, string2, string3, string4, string5, sep='\t')
+
+string_with_quotes = 'my string \''
+print(string_with_quotes)
+
+
