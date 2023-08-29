662fd16a86f43c634a5671ac6ea64f04f2c8f9a5
emanuel.craciun <emanuel.craciun@icloud.com>
2023-08-29 20:49:24 +0300

    modul 2

---
+++ modul2_2/app3.py
@@ -0,0 +1,7 @@
+#  01P - Andreea
+row = input('Introduceti un sir: ')
+print('Lungimea sirului este:', len(row))
+print('Lungimea sirului este: {}'.format(len(row)))
+print(f'Lungimea sirului este: {len(row)}')
+print('Lungimea sirului este: ' + str(len(row)))
+
