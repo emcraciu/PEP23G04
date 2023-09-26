efaf386b8c27165dc60b94a0b1ed8a0016cf5aea
emanuel.craciun <emanuel.craciun@icloud.com>
2023-09-26 20:56:12 +0300

    modul 5

--- modul5_2/app1.py
+++ modul5_2/app1.py
@@ -1,15 +1,53 @@
-STOCK = {'product': {100: 20, 101: 20}}
+STOCK = {'Mere': {100: 20, 101: 20}}
+
+
 def show_stock(product):
     pass
 
+
 def add_product(product, price, quantity):
-    pass
+    product, price, quantity = product.strip(), price.strip(), quantity.strip()
+    try:
+        merchendise = (STOCK[product])
+    except KeyError:
+        merchendise = STOCK[product] = {}
+
+    try:
+        stock = merchendise[int(price)]
+    except KeyError:
+        merchendise[int(price)] = int(quantity)
+    else:
+        merchendise[int(price)] = stock + int(quantity)
+
 
 def remove_product(product, price, quantity):
-    pass
+    product, price, quantity = product.strip(), price.strip(), quantity.strip()
+    try:
+        merchendise = (STOCK[product])
+    except KeyError:
+        print(f'Produsul introdus nu se afla in stoc')
+        return
+    try:
+        stock = merchendise[int(price)]
+    except KeyError:
+        print(f'{product} la pretul {price} nu se afla in stoc')
+        return
+    if stock < int(quantity):
+        print('Nu avem suficient stoc pentru comanda dorita')
+        return
+    merchendise[int(price)] = stock - int(quantity)
 
-def get_change_request():
-    input('Please provide: product, price, quantity')
+
+def change_request(option):
+    if option == '3':
+        response = input('Please provide: product, price, quantity')
+        data = response.split(',')
+        remove_product(*data)
+    elif option == '2':
+        response = input('Please provide: product, price, quantity')
+        data = response.split(',')
+        add_product(*data)
+
 
 while True:
     print('Meniu:')
@@ -22,6 +60,4 @@ while True:
     option = option.strip()
     if option == '4':
         exit()
-    elif option == '3':
-        get_change_request()
-
+    change_request(option)
