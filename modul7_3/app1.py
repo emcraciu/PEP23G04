ebcc34932a4d3a05d40db5317036cc584751a0c1
emanuel.craciun <emanuel.craciun@icloud.com>
2023-10-17 20:06:25 +0300

    modul 7

--- modul7_3/app1.py
+++ modul7_3/app1.py
@@ -1,5 +1,7 @@
 class Shop:
     categories = {'Haine': [], 'Accesorii': [], 'Imbracaminte': []}
+    first_menu_list = ['Categorii', 'Produse', 'Iesire']
+    product_menu_list = ['Adaugare', 'Vizualizare', 'Iesire']
 
     def first_menu(self):
         print('''Bun venit la magazinul PyCharm
@@ -17,6 +19,8 @@ class Shop:
         else:
             if option == 1:
                 self.category_menu()
+            elif option == 2:
+                self.product_menu()
 
     def category_menu(self):
         print(f' category '.center(80, '#'))
@@ -24,3 +28,35 @@ class Shop:
             print(f'--- {category}')
         input()
         self.first_menu()
+
+    def product_menu(self):
+        print(f' product '.center(80, '#'))
+        print('''Optiuni:
+        1.Adaugare
+        2.Vizualizare
+        3.Iesire
+    ''')
+        option = self.__get_option(self.product_menu, self.product_menu_list)
+        print(option)
+        if option == 3:
+            self.first_menu()
+
+    def __get_option(self, menu, menu_options):
+        option = input('Introduceti optiunea: ')
+        try:
+            option = int(option.strip())
+        except ValueError:
+            print('Option is not valid.')
+
+            return menu()
+
+        else:
+            if option not in range(1, len(menu_options) + 1):
+                print('Valoarea optiunii nu este valida:')
+                return menu()
+
+            return option
+
+
+s = Shop()
+s.first_menu()
---
+++ modul7_3/part1.py
@@ -0,0 +1,6 @@
+# enumerate
+
+
+my_list = ['a',  'b', 'c']
+for index, element in enumerate(my_list):
+    print(index + 1, element)
