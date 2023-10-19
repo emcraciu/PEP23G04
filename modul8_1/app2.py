614e945db8976f7c66ee2a6eb2247296b1865ee4
emanuel.craciun <emanuel.craciun@icloud.com>
2023-10-19 21:40:47 +0300

    modul 7

--- modul8_1/app2.py
+++ modul8_1/app2.py
@@ -15,7 +15,6 @@ class Masina:
         return Masina('', 0, '', 0, pret)
 
 
-
 masina1 = Masina("Audi", 4, "gri", 2006, 3400)
 masina2 = Masina("BMW", 2, "maro", 2007, 4788.60)
 masina3 = Masina("Volvo", 4, "gri", 2017, 27000)
@@ -26,13 +25,16 @@ masina7 = Masina("Volvo", 4, "gri", 2017, 27000)
 
 masini = [masina7, masina6, masina5, masina4, masina3, masina1, masina2]
 
-# avr = sum(masini)/len(masini)
+avr = sum(masini[1:], start=masini[0]).getPret() / len(masini)
+print(avr)
 
 x = masina1 + masina2 + masina3 + masina4 + masina5 + masina6 + masina7
-print(x.getPret()/len(masini))
+print(x.getPret() / len(masini))
+
 
 def my_map(masina: Masina):
     return masina.getPret()
+
 
 print(map(my_map, masini))
 print(list(map(my_map, masini)))
@@ -40,10 +42,23 @@ print(list(map(my_map, masini)))
 # Map
 
 # regular function
-avr = sum(map(my_map, masini))/len(masini)
+avr = sum(map(my_map, masini)) / len(masini)
 print(avr)
 
-#lambda function
-avr = sum(map(lambda masina: masina.getPret(), masini))/len(masini)
+# lambda function
+avr = sum(map(lambda masina: masina.getPret(), masini)) / len(masini)
 print(avr)
+
+# Filter
+result = filter(lambda masina: masina.getPret() > 10000, masini)
+print(result)
+
+# print(list(result)) # this will consume the filter that is a generator
+first_car = next(result)
+other_cars = list(result)
+
+print(sum(other_cars, start=first_car).getPret() / (len(other_cars) + 1))
+
+# BMW
+
 
